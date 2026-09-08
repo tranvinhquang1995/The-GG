72fa06414fd432493e8fe2c0d9b5c053a48cbfa6
Orin Tran <tranvinhquang1995@gmail.com>
2026-09-08 10:15:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ if df_games is not None:
        # if st.sidebar.button("📎 Attachment Center", use_container_width=True):
        #     st.session_state.app_mode = "📎 Attachment Center"
        #     st.rerun()
-        st.sidebar.button("📎 Attachment Center(Bảo trì)", use_container_width=True, disabled=True)
+        st.sidebar.button("📎 Attachment Center (Bảo trì)", use_container_width=True, disabled=True)
 
         # Danh sách các Portal duy nhất
         portals = sorted(df_games['Portal'].unique())
